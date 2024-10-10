328aaea440cc9b0cc8addc9e0c8e423fd5f15647
code-with-zulquar <codewithzulquar@gmail.com>
2024-10-10 19:56:54 +0530

    update

--- src/Toggle.jsx
+++ src/Toggle.jsx
@@ -1,24 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export default function Toggle() {
-    const [on, setOn] = useState(false);
-    const handleBtn = () => {
-        setOn(!on)
-    }
+  const [Toggle, setToggle] = useState(false);
+  const handleToggle = () => {
+    setToggle(!Toggle);
+  };
   return (
     <div
-      className=" w-full h-screen flex justify-center items-center "
-      onClick={handleBtn}
+      className="flex justify-center items-center w-full h-screen "
+      onClick={handleToggle}
     >
-      <div className="border-2 border-black bg-[#FFFF01] rounded-[40px] w-[20%]">
+      <div
+        className={`border-2 border-green-900  w-[20%] rounded-[40px] bg-[#FFFF01]`}
+      >
         <div
-          className={`border-2 border-black w-[25%] text-white duration-1000 rounded-full font-bold p-5 m-1 ${
-            on === false
-              ? "translate-x-0 bg-[#F00707] "
+          className={`w-[25%] font-bold  duration-1000 border-2 text-white border-black rounded-full p-5 m-1 ${
+            Toggle === false
+              ? "translate-x-0 bg-[#F00707]"
               : "translate-x-48 bg-green-500"
           }`}
         >
-          {on === false ? "OFF" : "On"}
+          {Toggle === false ? "OFF" : "On"}
         </div>
       </div>
     </div>
